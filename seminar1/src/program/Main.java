28e27ec545ad48f3d3d4437f0f31b38b2f51c112
StanSimona-Daniela <stansdaniela@gmail.com>
2021-02-28 16:28:16 +0200

    SECOND.2

--- seminar1/src/program/Main.java
+++ seminar1/src/program/Main.java
@@ -3,6 +3,14 @@ package program;
 public class Main {
 	public static void main(String[] args) {
 		
+		  Zoo zoo = new Zoo();
+	        Girafa g1 = new Girafa("Girafa 1");
+	        Girafa g2 = new Girafa("Girafa 2");
+
+	        zoo.Add(g1);
+	        zoo.Add(g2);
+
+	        zoo.feedAllAnimals();
 	}
 
 }
