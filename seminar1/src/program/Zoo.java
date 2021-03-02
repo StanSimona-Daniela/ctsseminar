b10aa99e44d92e8cc0fd2c572049e0aa6af0c059
StanSimona-Daniela <stansdaniela@gmail.com>
2021-03-02 08:45:57 +0200

    LIST

--- seminar1/src/program/Zoo.java
+++ seminar1/src/program/Zoo.java
@@ -1,18 +1,20 @@
 package program;
 
-import com.sun.tools.javac.util.List;
+import java.util.ArrayList;
+
 
 public class Zoo {
 
+	
 	private Zookeeper zookeeper;
-	private List<Animal> animals;
+	private ArrayList<Animal> animals;
 	
 	public Zoo() {
         this.zookeeper = new Zookeeper("Gigel");
         this.animals = new ArrayList<>();
     }
 
-    public Zoo(Zookeeper zookeeper, List<Animal> animalList) {
+    public Zoo(Zookeeper zookeeper, ArrayList<Animal> animalList) {
         this.zookeeper = zookeeper;
         this.animals = animalList;
     }
